e5faf13c8432134bd25e0cc29bb98451398566e7
Administrator <admin@example.com>
2020-12-22 15:03:14 +0800

    多线程版本

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
-use std::fs;
+use std::{fs, thread};
 
-fn main(){
-  let listener =   TcpListener::bind("127.0.0.1:7978").unwrap();
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7978").unwrap();
+    let mut thread_vec :Vec<thread::JoinHandle<()>> = Vec::new();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("连上了");
-        handle_connection(stream)
+        // println!("连上了");
+        // 创建线程
+     let handle =    thread::spawn(move || {
+            handle_connection(stream)
+        });
+        thread_vec.push(handle)
+    }
+    for handle in thread_vec{
+        handle.join().unwrap();
     }
 }
-fn handle_connection(mut stream:TcpStream){
-    let mut buffer = [0;1024];
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     let contents = fs::read_to_string("hello.html").unwrap();
     // let response = "HTTP/1.1 200 OK\r\n\r\n";
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",contents);
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
     // println!("request:{}",String::from_utf8_lossy(&buffer[..]));
